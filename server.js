57806943857dd51070a9ff07878cba7222ca91e9
Bret Doucette <doucette.bret@gmail.com>
2016-01-12 14:27:33 -0700

    Set up express server

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+               .listen(port, function(){
+                console.log('Listening on port' + port + '.');
+               });
+
+module.exports = server;
+
