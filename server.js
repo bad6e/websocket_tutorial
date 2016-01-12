f6a0390b7c943a668a21ca8efe2ea43f9b03a64c
Bret Doucette <doucette.bret@gmail.com>
2016-01-12 14:31:46 -0700

    Added websockets

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app)
                .listen(port, function(){
-                console.log('Listening on port' + port + '.');
+                console.log('Listening on port ' + port + '.');
                });
 
 module.exports = server;
+
+
+const socketIo = require ('socket.io');
+const io = socketIo(server);
+
+
 
